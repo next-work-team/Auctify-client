07f8309d3a7e1d045c691c68ffae8deb8518ab35
minpppal <minpal203@gmail.com>
2025-02-13 00:29:10 +0900

    KAN-7 <feat: 커밋테스트>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            111111111
           </a>
         </div>
       </main>
