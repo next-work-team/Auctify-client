7ce34e3b3c7b90aee416e04b6dcb6a512ffc31e1
강동욱 <ehddnr8563@gmail.com>
2025-02-20 23:39:19 +0900

    chore: vscode 설정 - 파일 저장시 포맷팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,28 +14,22 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     rules: {
-      // ✅ 기본 스타일 규칙
-      indent: ['error', 2], // 2칸 들여쓰기
-      quotes: ['error', 'single'], // 싱글 따옴표 사용
-      semi: ['error', 'always'], // 세미콜론 사용
-      'object-curly-spacing': ['error', 'always'], // 객체 중괄호 내부 공백 추가
-      'arrow-parens': ['error', 'always'], // 화살표 함수의 매개변수 괄호 항상 사용
-
-      // ✅ React 관련 규칙
+      // React 관련 규칙
+      'react/no-unescaped-entities': 'off',
       'react/jsx-filename-extension': [
         'error',
         { extensions: ['.tsx', '.jsx'] },
       ], // JSX는 .tsx, .jsx에서만 허용
-      'react/react-in-jsx-scope': 'off', // React import 필요 없음 (Next.js 환경 대응)
+      'react/react-in-jsx-scope': 'off', // React import 필요없음 (Next.js 환경 대응)
       'react/jsx-pascal-case': ['error', { allowAllCaps: true }], // 컴포넌트 이름 PascalCase 강제
 
-      // ✅ TypeScript 관련 규칙
+      // TypeScript 관련 규칙
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_' },
       ], // 사용되지 않는 변수 경고
 
-      // ✅ Import 정렬 관련 규칙
+      // Import 정렬 관련 규칙
       'import/order': [
         'error',
         {
