7b12bdf61a617d5e5ee74066dfa79dce1f04a840
minpppal <minpal203@gmail.com>
2025-03-02 02:37:33 +0900

    KAN-31 <feat: 구글 로그인 버튼 구현>

--- src/pages/sign-in/ui/GoogleLoginButton.tsx
+++ src/pages/sign-in/ui/GoogleLoginButton.tsx
@@ -5,7 +5,11 @@ import React from 'react';
 export default function GoogleLoginButton() {
   const handleLogin = () => {
     window.location.href =
-      'http://ec2-13-209-35-247.ap-northeast-2.compute.amazonaws.com:8080/';
+      'https://accounts.google.com/o/oauth2/auth?' +
+      'client_id=777440464902-90cdjve286mpm9rbs3eh7codssotpmsq.apps.googleusercontent.com&' +
+      'redirect_uri=백엔드uri' + // 백엔드 uri 필요
+      'response_type=code&' +
+      'scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'; //받아올 유저의 정보 범위
   };
 
   return <button onClick={handleLogin}>구글 로그인</button>;
