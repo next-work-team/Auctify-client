602a4a595d38cd1ea9ff5fb406bd5b7fc9029142
minpppal <minpal203@gmail.com>
2025-03-04 20:06:20 +0900

    KAN-31 <구글 로그인 redirect url 설정>

--- src/pages/sign-in/ui/GoogleLoginButton.tsx
+++ src/pages/sign-in/ui/GoogleLoginButton.tsx
@@ -5,11 +5,7 @@ import React from 'react';
 export default function GoogleLoginButton() {
   const handleLogin = () => {
     window.location.href =
-      'https://accounts.google.com/o/oauth2/auth?' +
-      'client_id=777440464902-90cdjve286mpm9rbs3eh7codssotpmsq.apps.googleusercontent.com&' +
-      'redirect_uri=백엔드uri' + // 백엔드 uri 필요
-      'response_type=code&' +
-      'scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'; //받아올 유저의 정보 범위
+      'http://13.209.35.247:8080/oauth2/authorization/google';
   };
 
   return <button onClick={handleLogin}>구글 로그인</button>;
