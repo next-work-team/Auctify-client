7218772e2f3063313d06694c19a269b75e72b2b7
강동욱 <ehddnr8563@gmail.com>
2025-03-06 15:59:29 +0900

    fix: 백엔드 api 도메인 수정

--- src/pages/sign-in/ui/KakaoLoginButton.tsx
+++ src/pages/sign-in/ui/KakaoLoginButton.tsx
@@ -2,7 +2,7 @@
 
 export default function KakaoLoginButton() {
   const handleKaKaoLogin = async () => {
-    window.location.href = `https://auctify.kro.kr/oauth2/authorization/kakao`;
+    window.location.href = `https://api.auctify.shop/oauth2/authorization/kakao`;
   };
 
   return <button onClick={handleKaKaoLogin}>카카오 로그인</button>;
