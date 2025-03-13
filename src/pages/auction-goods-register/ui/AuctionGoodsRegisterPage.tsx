e59482a715ce457b250c7a35b13e32a3a7afef45
강동욱 <ehddnr8563@gmail.com>
2025-03-13 23:21:27 +0900

    feat: 경매 등록 가이드 UI 구현

--- src/pages/auction-goods-register/ui/AuctionGoodsRegisterPage.tsx
+++ src/pages/auction-goods-register/ui/AuctionGoodsRegisterPage.tsx
@@ -1,3 +1,21 @@
+import AuctionGoodsRegisterHeader from './AuctionGoodsRegisterHeader';
+import AuctionGoodsRegisterForm from './AuctionGoodsRegisterForm';
+import AuctionGoodsRegisterGuide from './AuctionGoodsRegisterGuide';
+
 export function AuctionGoodsRegisterPage() {
-  return <>경매품 등록 페이지</>;
+  return (
+    <div className="container py-6 md:py-10">
+      <AuctionGoodsRegisterHeader />
+
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_350px] gap-6">
+        <div>
+          <AuctionGoodsRegisterForm />
+        </div>
+
+        <div className="hidden lg:block">
+          <AuctionGoodsRegisterGuide />
+        </div>
+      </div>
+    </div>
+  );
 }
